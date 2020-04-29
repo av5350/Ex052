b91173a9dcdb09df0c56748dfa5a6d504dcc4da9
av5350@bs.amalnet.k12.il <itayw5350>
2020-04-29 17:33:25 +0300

    end code

--- app/src/main/java/com/example/ex052/MainActivity.java
+++ app/src/main/java/com/example/ex052/MainActivity.java
@@ -15,7 +15,8 @@ public class MainActivity extends AppCompatActivity {
     EditText bValue;
     EditText cValue;
 
-    Button btn;
+    Button btnGraph;
+    Button btnClear;
 
     WebView wV;
 
@@ -28,13 +29,15 @@ public class MainActivity extends AppCompatActivity {
         bValue = (EditText)findViewById(R.id.bValue);
         cValue = (EditText)findViewById(R.id.cValue);
 
-        btn = (Button)findViewById(R.id.btn);
+        btnGraph = (Button)findViewById(R.id.btnGraph);
+        btnClear = (Button)findViewById(R.id.btnClear);
 
         wV = (WebView)findViewById(R.id.wV);
 
         wV.getSettings().setJavaScriptEnabled(true);
         wV.setWebViewClient(new MyWebViewClient());
 
+        wV.loadUrl("https://media.tenor.com/images/bdc934496f843d4863a960ca3ffd355f/tenor.gif");
     }
 
     public void showGraph(View view) {
@@ -53,6 +56,14 @@ public class MainActivity extends AppCompatActivity {
         else {
             wV.loadUrl("https://www.google.com/search?q=" + aString + "x%5E2%2B" + bString + "x%2B" + cString);
         }
+    }
+
+    public void clear(View view) {
+        aValue.setText("");
+        bValue.setText("");
+        cValue.setText("");
+
+        wV.loadUrl("https://media.tenor.com/images/bdc934496f843d4863a960ca3ffd355f/tenor.gif");
     }
 
     private class MyWebViewClient extends WebViewClient {
